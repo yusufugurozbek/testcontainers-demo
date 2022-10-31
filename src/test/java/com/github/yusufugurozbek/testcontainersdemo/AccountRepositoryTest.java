960a4d8ec44ed2c8e7715dd704d9ea0ab409e133
Yusuf Ugur ÖZBEK <yusufugurozbek@hotmail.com>
2022-10-31 21:41:41 +0100

    Add tests for getAge and getTotalAmount

--- src/test/java/com/github/yusufugurozbek/testcontainersdemo/AccountRepositoryTest.java
+++ src/test/java/com/github/yusufugurozbek/testcontainersdemo/AccountRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
@@ -36,6 +37,37 @@ class AccountRepositoryTest {
 
         // then
         assertEquals("Count is incorrect", 1, accounts.size());
+    }
+
+    @Test
+    void getAge_returnsCorrectAge() {
+        // given
+        var expectedAge = 35;
+        var account = new Account("Yusuf Ugur", "ÖZBEK", new BigDecimal(1000000), LocalDate.now().minusYears(expectedAge));
+        var savedAccount = accountRepository.save(account);
+
+        // when
+        var actualAge = accountRepository.getAgeById(savedAccount.getId());
+
+        // then
+        assertEquals("Age is incorrect", expectedAge, actualAge);
+    }
+
+    @Test
+    void getTotalAmount_returnsCorrectAmount() {
+        // given
+        var accounts = List.of(
+                new Account("Yusuf Ugur", "ÖZBEK", new BigDecimal(1000000), LocalDate.now().minusYears(35)),
+                new Account("Yusuf", "ÖZBEK", new BigDecimal(100000), LocalDate.now().minusYears(45)),
+                new Account("Ugur", "ÖZBEK", new BigDecimal(10000), LocalDate.now().minusYears(55))
+        );
+        accountRepository.saveAll(accounts);
+
+        // when
+        var totalAmount = accountRepository.getTotalAmount();
+
+        // then
+        assertEquals("Total amount is incorrect", new BigDecimal(1110000), totalAmount);
     }
 
 }
