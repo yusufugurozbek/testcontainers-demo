59713834ee9e5b684755729a24ddf1d8792bebfb
Yusuf Ugur ÖZBEK <yusufugurozbek@hotmail.com>
2022-12-24 17:57:05 +0100

    Bump Spring Boot from 2.7.5 to 3.0.1 :arrow_up:

--- src/main/java/com/github/yusufugurozbek/testcontainersdemo/Account.java
+++ src/main/java/com/github/yusufugurozbek/testcontainersdemo/Account.java
@@ -1,10 +1,10 @@
 package com.github.yusufugurozbek.testcontainersdemo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
